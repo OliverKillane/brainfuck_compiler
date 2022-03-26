708a5258ac0f60092da542a69616f75b122b4226
Oliver Killane <oliver.killane20@imperial.ac.uk>
2022-03-26 21:56:07 +0000

    Added basic cli for using the compiler

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,71 @@ mod arch;
 mod intermediate;
 mod parser;
 
+use std::{
+    fs::{read_to_string, File},
+    io::Write,
+    path::PathBuf,
+    process::exit,
+};
+
+use clap::Parser;
+#[derive(Parser)]
+#[clap(author = "Oliver Killane", about = "BrainFuck compiler" , long_about = Some("A brainfuck compiler targeting multiple architectures"), version = "0.0.1")]
+struct Args {
+    #[clap(parse(from_os_str), value_name = "FILE")]
+    inputpath: PathBuf,
+
+    #[clap(
+        short,
+        long,
+        parse(from_os_str),
+        value_name = "FILE",
+        help = "The name of the output file"
+    )]
+    outputpath: Option<PathBuf>,
+}
+
+const EXIT_SUCCESS: i32 = 0;
+const FILE_READ_FAILURE: i32 = 1;
+const FILE_WRITE_FAILURE: i32 = 2;
+const FILE_CREATE_FAILURE: i32 = 3;
+const SYNTAX_ERROR: i32 = 4;
+
 fn main() {
-    todo!()
+    let Args {
+        mut inputpath,
+        outputpath,
+    } = Args::parse();
+
+    match read_to_string(inputpath.clone()) {
+        Ok(_source) => {
+            // todo parsing
+
+            // todo optimisation
+
+            // todo generation
+
+            let mut outputfile = if let Ok(file) = match outputpath {
+                Some(path) => File::create(path),
+                None => {
+                    inputpath.set_extension(".s");
+                    File::create(inputpath)
+                }
+            } {
+                file
+            } else {
+                exit(FILE_CREATE_FAILURE)
+            };
+
+            if outputfile.write("this is the code".as_bytes()).is_err() {
+                exit(FILE_WRITE_FAILURE);
+            }
+
+            exit(EXIT_SUCCESS)
+        }
+        Err(_) => {
+            println!("Unable to open file");
+            exit(FILE_READ_FAILURE)
+        }
+    }
 }
